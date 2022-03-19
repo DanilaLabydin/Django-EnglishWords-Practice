2bd6892fe2e35fa09ee2d9333e86213710eec0ee
danilalabydindfsdf <labydin2605@mail.ru>
2022-03-19 17:47:25 +0300

    rebuild practice template,I still have no idea how to add the multiediting

--- english_words_app/urls.py
+++ english_words_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("dictionary_desc/", views.dictionary_desc, name='dictionary_desc'),
     path("new_word/", views.new_word, name='new_word'),
     path('words_practice/', views.words_practice, name='words_practice'),
-    path('words_practice1/', views.words_practice1, name='words_practice1'),
-    path('test_func/<int:word_id>/', views.test_fun, name='test_func'),
+    path('words_practice1/<int:word_id>/', views.words_practice1, name='words_practice1'),
+    path('word_update/', views.WordUpdate.as_view(), name='word-update'),
 ]
--- english_words_app/views.py
+++ english_words_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.shortcuts import render, redirect, reverse
+from django.views.generic import ListView, UpdateView
 from .models import EnglishWord
 from .forms import WordForm, WordPracticeForm
 
@@ -39,14 +39,16 @@ def words_practice(request):
     return render(request, 'english_words_app/words_practice.html', {'words': words, 'form': form})
 
 
-def words_practice1(request):
+def words_practice1(request, word_id):
     words = EnglishWord.objects.all()
+
     if request.method != 'POST':
         form = WordPracticeForm()
     else:
         form = WordForm(data=request.POST)
         if form.is_valid():
             print(form.cleaned_data['translation'])
+            form.save()
     return render(request, 'english_words_app/words_practice1.html', {'words': words, 'form': form})
 
 
@@ -64,7 +66,37 @@ def test_fun(request, word_id):
     return render(request, 'english_words_app/test.html', context)
 
 
+class WordUpdate(UpdateView):
+    model = EnglishWord
+    fields = ['translation']
+
+    def get_context_data(self, **kwargs):
+        context = super(WordUpdate, self).get_context_data()
+        return context
+
+    def get_success_url(self):
+        return reverse('dictionary')
+
 """
+class WorldUpdate(UpdateView):
+    model = ToDoItem
+    fields = [
+        'todo_list',
+        'title',
+        'description',
+        'due_time',
+    ]
+
+    def get_context_data(self, **kwargs):
+        context = super(ItemUpdate, self).get_context_data()
+        context['todo_list'] = self.object.todo_list
+        context['title'] = 'Edit item'
+        return context
+
+    def get_success_url(self):
+        return reverse('list', args=[self.object.todo_list_id])
+
+
 def words_practice1(request):
     words = EnglishWord.objects.all()
     for word in words:
